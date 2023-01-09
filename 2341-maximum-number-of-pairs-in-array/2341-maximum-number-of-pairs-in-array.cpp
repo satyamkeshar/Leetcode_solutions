3e9d6d41071f6b244a2d38be0dd6bbe1a6f6b051
Satyam keshar <107239908+satyamkeshar@users.noreply.github.com>
2023-01-09 17:45:20 +0530

    Time: 4 ms (62.15%), Space: 9.4 MB (32.24%) - LeetHub

---
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> numberOfPairs(vector<int>& nums) {
+        int pair =0;
+        int left =0;
+       unordered_map<int, int> umap;
+        vector<int> nums1;
+    for(auto i : nums)
+    {
+        umap[i]++;
+        
+    }
+        
+        for( auto it: umap)
+        {
+            if(it.second ==2)
+            {
+                pair++;
+            }
+            else if(it.second>2)
+            {
+                pair += it.second/2;
+                if(it.second%2 !=0)
+                left++;
+            }
+            else if(it.second==1)
+                left++;
+            // nums.erase(it);
+        }
+        
+        nums1.push_back(pair);
+        nums1.push_back(left);
+        return nums1;
+    }
+};
