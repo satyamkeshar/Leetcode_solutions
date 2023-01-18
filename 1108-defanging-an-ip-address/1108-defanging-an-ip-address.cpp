80f057ebdba371c81eb4479b25bebc7d75d5de63
Satyam keshar <107239908+satyamkeshar@users.noreply.github.com>
2023-01-18 23:50:05 +0530

    Time: 0 ms (100.00%), Space: 6 MB (28.60%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+           string ans ="";
+
+        for(char x:address)
+        {
+            if(x =='.')
+               ans = ans + "[.]";
+            else
+               ans = ans + x; 
+        }
+        return ans;
+    }
+};
