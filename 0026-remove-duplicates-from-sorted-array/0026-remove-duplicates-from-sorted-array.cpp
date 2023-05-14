d41fd185f15d21e25e08ed4af6df7e4cda52d46c
Satyam keshar <107239908+satyamkeshar@users.noreply.github.com>
2023-05-14 19:28:37 +0530

    Time: 12 ms (62.85%), Space: 18.4 MB (75.30%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,19 +1,43 @@
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        set<int> s; 
-        for(int i =0; i<nums.size(); i++)
-        {
-            s.insert(nums[i]);
-        }
+// class Solution {
+// public:
+// //     int removeDuplicates(vector<int>& nums) {
+//         set<int> s; 
+//         for(int i =0; i<nums.size(); i++)
+//         {
+//             s.insert(nums[i]);
+//         }
         
-        int ans = s.size();
-        nums.clear();
-        for(auto i:s)
+//         int ans = s.size();
+//         nums.clear();
+//         for(auto i:s)
+//         {
+//            //int k = *i;
+//            nums.push_back(i);
+//         }
+//         return ans;
+    class Solution {
+public:
+    int removeDuplicates(vector<int>& nums)
+    {
+        int newindex=0;
+        int length=nums.size();
+        if(nums.size()==1|| nums.size()==0)
         {
-           //int k = *i;
-           nums.push_back(i);
+            return nums.size();
         }
-        return ans;
+        int iterating_index=0;
+        for(int iterating_index=1; iterating_index<nums.size(); iterating_index++)
+        {
+            if(nums[newindex]!=nums[iterating_index])
+            {
+                newindex++;
+                nums[newindex]=nums[iterating_index];
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return newindex+1;
     }
 };
