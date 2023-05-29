370ce8880f717035fdd0fb67b62d931951dc82e6
Satyam keshar <107239908+satyamkeshar@users.noreply.github.com>
2023-05-29 20:24:09 +0530

    Time: 19 ms (75.74%), Space: 19.7 MB (23.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,22 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int >umap;
-        
-        for(int i =0; i<nums.size(); i++)
-        {
-            umap[nums[i]]++;
-        }
-        
-        for(auto i: umap)
-        {
-            if(i.second>nums.size()/2)
-            {
-                return i.first;
-            }
-        }
-        
-        return 0;
+      sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
     }
-    
 };
